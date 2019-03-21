3d3378a6f940e41bf317e2ad42729ac4cc5ac100
Walee Ahmed <dev@walee.me>
2019-03-21 18:01:18 -0400

    migrated api to use dart

--- api/functions/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { https } from "firebase-functions"
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-
-export const helloWorld = https.onRequest((request, response) => {
-  response.send("Hello from Firebase!")
-})
