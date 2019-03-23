210d1ee6bd4a727a28e1b62bb4dcb2bf80e162ca
Walee Ahmed <dev@walee.me>
2019-03-22 22:02:54 -0400

    cleaner bottom bar handling using useState

--- mobile/lib/app.dart
+++ mobile/lib/app.dart
@@ -1,48 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:rallii/widgets/bottom_navigation.dart';
 import 'package:rallii/widgets/tab_navigator.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class App extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => AppState();
-}
-
-class AppState extends State<App> {
-  TabItem _currentTab = TabItem.quest;
-  Map<TabItem, GlobalKey<NavigatorState>> navigatorKeys = {
+class App extends HookWidget {
+  final Map<TabItem, GlobalKey<NavigatorState>> navigatorKeys = {
     TabItem.quest: GlobalKey<NavigatorState>(),
     TabItem.message: GlobalKey<NavigatorState>(),
     TabItem.profile: GlobalKey<NavigatorState>(),
   };
 
-  void _selectTab(TabItem tabItem) {
-    setState(() => _currentTab = tabItem);
-  }
-
   @override
   Widget build(BuildContext context) {
+    final _currentTab = useState(TabItem.quest);
+
     return WillPopScope(
       onWillPop: () async =>
           !await navigatorKeys[_currentTab].currentState.maybePop(),
       child: Scaffold(
         body: Stack(
           children: <Widget>[
-            _buildOffstageNavigator(TabItem.message),
-            _buildOffstageNavigator(TabItem.profile),
-            _buildOffstageNavigator(TabItem.quest),
+            _buildOffstageNavigator(TabItem.message, _currentTab.value),
+            _buildOffstageNavigator(TabItem.profile, _currentTab.value),
+            _buildOffstageNavigator(TabItem.quest, _currentTab.value),
           ],
         ),
         bottomNavigationBar: BottomNavigation(
-          currentTab: _currentTab,
-          onSelectTab: _selectTab,
+          currentTab: _currentTab.value,
+          onSelectTab: (TabItem tabItem) => _currentTab.value = tabItem,
         ),
       ),
     );
   }
 
-  Widget _buildOffstageNavigator(TabItem tabItem) {
+  Widget _buildOffstageNavigator(TabItem tabItem, TabItem currentTab) {
     return Offstage(
-      offstage: _currentTab != tabItem,
+      offstage: currentTab != tabItem,
       child: TabNavigator(
         navigatorKey: navigatorKeys[tabItem],
         tabItem: tabItem,
