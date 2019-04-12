f4504ff0332e35d37a6510788cf09d1cba787feb
Walee Ahmed <dev@walee.me>
2019-04-12 00:11:30 -0400

    moved functions to TS

--- api/functions/src/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:firebase_functions_interop/firebase_functions_interop.dart';
-
-import "misc.dart";
-
-void main() {
-  functions['helloWorld'] = functions.https.onRequest(helloWorld);
-}
--- api/functions/src/misc.dart
+++
@@ -1,6 +0,0 @@
-import 'package:firebase_functions_interop/firebase_functions_interop.dart';
-
-void helloWorld(ExpressHttpRequest request) {
-  request.response.writeln('Hello world');
-  request.response.close();
-}
