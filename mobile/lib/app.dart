99168106b0f812d48354480304c4f00633aa5bd8
Walee Ahmed <dev@walee.me>
2019-04-13 13:48:35 -0400

    removed pageview

--- mobile/lib/app.dart
+++ mobile/lib/app.dart
@@ -5,12 +5,11 @@ import './widgets/bottom_navigation.dart';
 import './widgets/tab_navigator.dart';
 
 class App extends HookWidget {
-  final Map<TabItem, GlobalKey<NavigatorState>> _navigatorKeys = {
+  final Map<TabItem, GlobalKey<NavigatorState>> navigatorKeys = {
     TabItem.quest: GlobalKey<NavigatorState>(),
     TabItem.message: GlobalKey<NavigatorState>(),
     TabItem.profile: GlobalKey<NavigatorState>(),
   };
-  final PageController _controller = new PageController(keepPage: true);
 
   @override
   Widget build(BuildContext context) {
@@ -18,32 +17,30 @@ class App extends HookWidget {
 
     return WillPopScope(
       onWillPop: () async =>
-          !await _navigatorKeys[_currentTab].currentState.maybePop(),
+          !await navigatorKeys[_currentTab].currentState.maybePop(),
       child: Scaffold(
-        body: PageView(
-          controller: _controller,
+        body: Stack(
           children: <Widget>[
-            _buildOffstageNavigator(TabItem.quest, _currentTab.value),
             _buildOffstageNavigator(TabItem.message, _currentTab.value),
             _buildOffstageNavigator(TabItem.profile, _currentTab.value),
+            _buildOffstageNavigator(TabItem.quest, _currentTab.value),
           ],
         ),
         bottomNavigationBar: BottomNavigation(
           currentTab: _currentTab.value,
-          onSelectTab: (TabItem tabItem) {
-            _controller.animateToPage(tabItem.index,
-                duration: Duration(milliseconds: 200), curve: Curves.ease);
-            _currentTab.value = tabItem;
-          },
+          onSelectTab: (TabItem tabItem) => _currentTab.value = tabItem,
         ),
       ),
     );
   }
 
   Widget _buildOffstageNavigator(TabItem tabItem, TabItem currentTab) {
-    return TabNavigator(
-      navigatorKey: _navigatorKeys[tabItem],
-      tabItem: tabItem,
+    return Offstage(
+      offstage: currentTab != tabItem,
+      child: TabNavigator(
+        navigatorKey: navigatorKeys[tabItem],
+        tabItem: tabItem,
+      ),
     );
   }
 }
