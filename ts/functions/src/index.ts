6e3f829d9fa4122d4cd19a18a228956bfcda67ee
Walee Ahmed <dev@walee.me>
2019-04-14 22:17:00 -0400

    reworked repo structure into proper monorepo

--- ts/functions/src/index.ts
+++ ts/functions/src/index.ts
@@ -1,10 +1,10 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = functions.https.onRequest(
-  (request, response): void => {
-    response.send("Hello from Firebase!");
-  }
+    (request, response): void => {
+        response.send('Hello from Firebase!');
+    },
 );
