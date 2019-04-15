c63cfe6a5d7e37b7fdc4e42913c14a6c5ed5be10
Walee Ahmed <dev@walee.me>
2019-04-14 22:22:55 -0400

    tweak

--- ts/functions/src/index.ts
+++ ts/functions/src/index.ts
@@ -1,9 +1,9 @@
-import * as functions from 'firebase-functions';
+import { https } from 'firebase-functions';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest(
+export const helloWorld = https.onRequest(
     (request, response): void => {
         response.send('Hello from Firebase!');
     },
