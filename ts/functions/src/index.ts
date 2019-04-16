7d4231597640dc0517a8e121677fd915dac7f749
Walee Ahmed <dev@walee.me>
2019-04-15 22:13:16 -0400

    tweaked prettier options & eslint integration

--- ts/functions/src/index.ts
+++ ts/functions/src/index.ts
@@ -1,10 +1,10 @@
-import { https } from 'firebase-functions';
+import { https } from 'firebase-functions'
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = https.onRequest(
-    (request, response): void => {
-        response.send('Hello from Firebase!');
-    },
-);
+  (request, response): void => {
+    response.send('Hello from Firebase!')
+  }
+)
