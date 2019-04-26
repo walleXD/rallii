8dd17e63c0935c1a06a856d92b37ff480ea2ca98
Walee Ahmed <dev@walee.me>
2019-04-26 15:55:38 -0400

    added simple widget test

--- dart/mobile/lib/main.dart
+++ dart/mobile/lib/main.dart
@@ -5,9 +5,12 @@ import "src/app.dart";
 void main() => runApp(AppRoot());
 
 class AppRoot extends StatelessWidget {
+  AppRoot({key});
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      key: key,
       title: 'Rallii',
       theme: ThemeData(
         primarySwatch: Colors.blue,
