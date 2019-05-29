fc4554eb82b8f31c7d18acaf242425b903cd570b
Walee Ahmed <dev@walee.me>
2019-05-29 07:28:46 -0400

    added landing project

---
+++ js/landing/src/components/hero.js
@@ -0,0 +1,63 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import Button from "../components/button"
+import headerImage from "../images/header.png"
+import MockupContent from "./image"
+import mockupFrame from "../images/mockup-frame.png"
+
+const Header = ({ siteTitle }) => (
+  <div
+    style={{
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column",
+      padding: "4rem 1rem",
+    }}
+  >
+    <div
+      style={{
+        backgroundImage: `url(${headerImage})`,
+        position: "absolute",
+        top: 0,
+        zIndex: -5,
+        height: "100vh",
+        width: "100vw",
+        opacity: 0.5,
+      }}
+    />
+    <h1 style={{ textAlign: "center" }}>Landing Page Starter</h1>
+    <p style={{ textAlign: "center", maxWidth: 440 }}>
+      This landing page looks great on all devices and is minimal in design. Add
+      what you want and deploy.
+    </p>
+    <Button>Get Early Access</Button>
+    <div style={{ margin: 60, width: `250px`, position: "relative" }}>
+      <div style={{ clipPath: "inset(2% 5% round 2% 5%)" }}>
+        <MockupContent />
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          width: "250px",
+          top: 0,
+        }}
+      >
+        <img
+          src={mockupFrame}
+          alt="outlines of shapes and confetti in the background "
+        />
+      </div>
+    </div>
+  </div>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
